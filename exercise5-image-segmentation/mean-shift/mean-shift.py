87784e01601ee578cb8fab7a068864da0847512c
oalkayacd <oalkaya@ethz.ch>
2023-11-09 19:18:46 +0100

    finish mean shift

--- exercise5-image-segmentation/mean-shift/mean-shift.py
+++ exercise5-image-segmentation/mean-shift/mean-shift.py
@@ -5,39 +5,64 @@ import numpy as np
 from skimage import io, color
 from skimage.transform import rescale
 
-def distance(x, X):
-    raise NotImplementedError('distance function not implemented!')
 
-def gaussian(dist, bandwidth):
-    raise NotImplementedError('gaussian function not implemented!')
+# Changed function signature to compute vectorized distances
+def distance(X):
+    return np.sqrt(((X[:, np.newaxis] - X) ** 2).sum(axis=2)) # Vectorized computation of all pairwise distances
 
-def update_point(weight, X):
-    raise NotImplementedError('update_point function not implemented!')
+def gaussian(dists, bandwidth):
+    return np.exp(-0.5 * (dists / bandwidth) ** 2)
+
+def update_point(weights, X):
+    weighted_sum = np.dot(weights, X)
+    sum_of_weights = weights.sum(axis=1, keepdims=True)
+    return weighted_sum / sum_of_weights
 
 def meanshift_step(X, bandwidth=2.5):
-    raise NotImplementedError('meanshift_step function not implemented!')
+    dists = distance(X)                     # Use the vectorized distance function
+    weights = gaussian(dists, bandwidth)    # Use the gaussian function
+    new_X = update_point(weights, X)        # Use the vectorized update function
+    return new_X                            # Return new X
 
-def meanshift(X):
-    for _ in range(20):
-        X = meanshift_step(X)
+
+def meanshift(X, bandwidth, num_labels):
+    # Set number of centroids to num of pixels at the start (set it to some large value)
+    num_centroids = X.shape[0]
+    step_count = 0
+    while True:
+        X = meanshift_step(X, bandwidth)
+        # Check for convergence behaviour via centroids
+        centroids, labels = np.unique((X / 4).round(), return_inverse=True, axis=0)
+        num_centroids = centroids.shape[0]
+        print("Step " + str(step_count+1) + " (centroids=" + str(num_centroids) + ")")
+        if(num_centroids <= num_labels):
+            break
+        else:
+            step_count += 1
     return X
 
-scale = 0.5    # downscale the image to run faster
+# Hyperparameters
+scale = 0.5         # downscale the image to run faster
+bandwidth = 3       # bandwitdth for gaussians
 
 # Load image and convert it to CIELAB space
-image = rescale(io.imread('eth.jpg'), scale, channel_axis=-1)
+image = rescale(io.imread('exercise5-image-segmentation/mean-shift/eth.jpg'), scale, channel_axis=-1)
 image_lab = color.rgb2lab(image)
 shape = image_lab.shape # record image shape
 image_lab = image_lab.reshape([-1, 3])  # flatten the image
 
+# Load label colours
+colors = np.load('exercise5-image-segmentation/mean-shift/colors.npz')['colors']
+
 # Run your mean-shift algorithm
 t = time.time()
-X = meanshift(image_lab)
+X = meanshift(image_lab, bandwidth, colors.shape[0]) # Use number of label colours as loop condition within mean shift
 t = time.time() - t
 print ('Elapsed time for mean-shift: {}'.format(t))
 
-# Load label colors and draw labels as an image
-colors = np.load('colors.npz')['colors']
+
+
+# Draw labels as an image
 colors[colors > 1.0] = 1
 colors[colors < 0.0] = 0
 
@@ -46,4 +71,4 @@ centroids, labels = np.unique((X / 4).round(), return_inverse=True, axis=0)
 result_image = colors[labels].reshape(shape)
 result_image = rescale(result_image, 1 / scale, order=0, channel_axis=-1)     # resize result image to original resolution
 result_image = (result_image * 255).astype(np.uint8)
-io.imsave('result.png', result_image)
+io.imsave('exercise5-image-segmentation/mean-shift/result.jpg', result_image)
